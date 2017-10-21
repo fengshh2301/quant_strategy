215b6b8b0ba951c2f632a6641862cab9a60209fa
jasonfeng <fengshh2301@sina.com>
2017-10-21 21:59:26 +0800

    mod file tittle

--- targetver.h
+++ targetver.h
@@ -1,8 +1,18 @@
-#pragma once
+/*************************************************************************
+* File Name  : targetver.h
+* Description:
+* 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
+* 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
+* 将 _WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+* Author     : Jason Feng
+* Email      : fengshh2301@sina.com
+* Time       : 10/21/2017 21:56:27
+ ************************************************************************/
+#ifndef _TARGETVER_H_
 
-// 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
-
-// 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
-// 将 _WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+#define _TARGETVER_H_
 
 #include <SDKDDKVer.h>
+
+#endif /* end of include guard: _TARGETVER_H_ */
+
