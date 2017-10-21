38ca488a56d6472c73a8c9603a0f88f48dd7ff09
jasonfeng <fengshh2301@sina.com>
2017-10-21 21:37:30 +0800

    mod file tittle

--- stdafx.h
+++ stdafx.h
@@ -1,17 +1,24 @@
-// stdafx.h : 标准系统包含文件的包含文件，
-// 或是经常使用但不常更改的
-// 特定于项目的包含文件
-//
+/*************************************************************************
+* File Name  : stdafx.h
+* Description:
+* Author     : Jason Feng
+* Email      : fengshh2301@sina.com
+* Time       : 10/21/2017 21:36:11
+ ************************************************************************/
 
-#pragma once
+#ifndef STDAFX_H
+
+#define STDAFX_H
 
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
-// Windows 头文件: 
+// Windows 头文件:
 #include <windows.h>
 #include <iostream>
 
-
-
 // TODO:  在此处引用程序需要的其他头文件
+
+
+#endif /* end of include guard: STDAFX_H */
+
